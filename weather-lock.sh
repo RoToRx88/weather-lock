54e5122993b9c067a740310720dc774b8574c9cb
Nicolas <comte_a@epitech.eu>
2019-03-27 12:14:02 +0100

    fix: Fix condition + add base image existence check

--- weather-lock.sh
+++ weather-lock.sh
@@ -2,13 +2,19 @@
 
 # To generate background if lost: convert -size 1920x2160 xc:black background.png
 
+if [ ! -f /tmp/background_lock.png ]
+then
+  convert -size 1920x2160 xc:black /tmp/background_lock.png
+fi
+
 if [ test $(find /tmp/lock.png -mmin +240) ] || [ ! -f /tmp/lock.png ]
 then
-  printf 'Generating files...'
-  convert -size 1920x2160 xc:black /tmp/background_lock.png
+  printf "Last update less than 2h ago"
+else
+  printf 'Generating file...'
   curl wttr.in/lyon.png --output /tmp/lyon.png
   curl wttr.in/moon.png --output /tmp/moon.png
-  composite -gravity north /tmp/lyon.png /tmp/background_lock.png /tmp/lock.png
+  composite -gravity north /tmp/lyon.png /home/ncomte/Pictures/background_lock.png /tmp/lock.png
   composite -gravity south /tmp/moon.png /tmp/lock.png /tmp/lock.png
 fi
 i3lock -c 000000 -e -f -i /tmp/lock.png
